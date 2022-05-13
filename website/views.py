1753dd6436bc595714ef2fb4735cfc39716fa4f8
sblekic <blekic.sandi@gmail.com>
2022-05-13 14:13:20 +0200

    test, dohvacanje podataka iz html form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 # views/url endpoints
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # blueprint mi omogucava podjelu route-ova u više datoteka, da ne moram sve ovdje natrpati
 
@@ -11,6 +11,9 @@ def home():
     return render_template("dashboard.html")
 
 
-@views.route('/namirnice')
+@views.route('/namirnice', methods=['GET', 'POST'])
 def namirnice():
+    if request.method == 'POST':
+        ime_namirnice = request.form.get('imeNamirnice')
+        print(ime_namirnice)
     return render_template("namirnice.html")
